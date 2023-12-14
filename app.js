b4b276dcbaf52aa32f2e89c071679bd633fa662a
NikitaKHS <151582375+NikitaKHS@users.noreply.github.com>
2023-12-14 17:54:54 +0300

    Jenkins help

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
  /_____/   U
 `;
 
-    res.send(`<pre>${dogArt}</pre><p>Это собачка</p>`);
+    res.send(`<pre>${dogArt}</pre><p>Это собачка,da</p>`);
 });
 
 app.listen(3000, () => {
